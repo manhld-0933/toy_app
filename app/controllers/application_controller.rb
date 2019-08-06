752b3c9d91d3d5d84a6a6d0bc20bf1ab727fabee
Le Duc Manh <le.duc.manh@sun-asterisk.com>
2019-08-06 10:39:03 +0700

    Finish toy app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
   def hello
-    render html: "hello, world!"
+    render html: "hello, world!A"
   end
 
 end
